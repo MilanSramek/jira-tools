3311448d8d7488fb6b4326a2ac12d64b5a8735f9
Milan Šrámek <milan.sramek.ml@gmail.com>
2026-04-30 18:00:48 +0200

    chore: update target framework and package versions

--- src/JiraTools/Program.cs
+++ src/JiraTools/Program.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-// args = ["timesheet", "import", "--source", "Clockify", "--period", "Month", "--anchor", "2026-04-30"];
+args = ["timesheet", "import", "--source", "Clockify", "--period", "Month", "--anchor", "2026-04-30"];
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureAppConfiguration((context, config) =>
     {
